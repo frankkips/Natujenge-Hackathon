08485cc271e15ee414fb0d6aa82be8304628c3ec
frankkips <franklinekiplagat1@gmail.com>
2022-05-19 18:13:17 +0300

    1st commmit

--- src/com/frankkips/Main.java
+++ src/com/frankkips/Main.java
@@ -3,6 +3,31 @@ package com.frankkips;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	byte[] bytes = new byte[]{0x13, 0x55, (byte)0xb6, 0x76,
+            0x79, (byte)0x88, 0x29, 0x5e,
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x03, 0x00, 0x34,
+            0x7e, 0x58, 0x1e, 0x36,
+            0x00, 0x00, 0x00, 0x00,
+            0x00, 0x00, 0x00, 0x00};
+// Source Port
+        int source = 0;
+        source |= (bytes[0]& 0xff) << 8;
+        source|= (bytes[1] & 0xff);
+        System.out.println("source port "+ source);
+
+//        Destination port
+        int dest = 0;
+        dest |= (bytes[2]& 0xff) << 8;
+        dest|= (bytes[3] & 0xff);
+        System.out.println("Destination port "+ dest);
+
+
+//        Verification Tag
+//        0x79, 0x88, 0x29, 0x5e
+        int verificate =
+
+
+
     }
 }
